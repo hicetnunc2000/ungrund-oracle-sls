0342e24f0cf98a0e4eb23c62cfa52ce14910fae4
crzypatchwork <180111973@aluno.unb.br>
2021-01-20 15:15:43 -0300

    0.9.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# ungrund-NaaS
+```
+such service intends to feed data by decentralized means on chain for smart contracts usage. v1 presents a simple structure for requesting natural numbers from a known API. ungrund oracle reads state changes on chain and provides data asked.
+it's intended to implement support for string and bytes response types, flux aggregators and possibly token economy. 
+```
+
+## Delphinet
+```
+KT1UmGvGnewwhrZGKkfJ9cZKdjcwz5rsDpg6
+```
+
+## .env
+```
+SK=<TEZOS SECRET KEY>
+```
+
+```MIT License```
---
+++ handler.js
@@ -0,0 +1,96 @@
+require('dotenv').config()
+
+const { TezosToolkit } = require('@taquito/taquito')
+const { InMemorySigner } = require('@taquito/signer')
+const axios = require('axios')
+const _ = require('lodash')
+
+const ungrund = 'KT1UmGvGnewwhrZGKkfJ9cZKdjcwz5rsDpg6'
+const Tezos = new TezosToolkit('https://api.tez.ie/rpc/delphinet')
+
+Tezos.setProvider({
+	signer : new InMemorySigner(process.env.SK)
+})
+
+// FUNCTIONS
+
+// key management
+
+// fail configuration
+
+const inject = async (reqId, result) => {
+  console.log([reqId, result])
+  try {
+    let kt = await Tezos.contract.at(ungrund)
+    let op = await kt.methods.fullFillRequest(reqId, result).send()
+  } catch (e) {
+    console.log(e)
+  }
+
+}
+
+//inject()
+// requests link and path from storage for such stateId
+const getStorage = async (network, kt) => {
+  let ptr = await axios.get(`https://api.better-call.dev/v1/contract/${network}/${kt}/storage`).then(res => res.data.children[0].value)
+  let bigMap = await axios.get(`https://api.better-call.dev/v1/bigmap/${network}/${ptr}/keys`).then(res => res.data)
+  let keys = bigMap.map(e => e.data).filter(e => e.value.children[1].value == false).map(e => parseInt(e.key.value)).sort()
+  console.log(keys)
+  let callback = bigMap.filter(e => e.data.key.value == keys[0])[0].data.value.children[0].value
+  let payload = bigMap.filter(e => e.data.key.value == keys[0])[0].data.value.children[2].children.map(e => e.value)
+  console.log(callback) //verify if callback is configured with proper parameters entrypoint
+  console.log(payload)
+  //verify if args follows a pattern
+  return [keys[0], payload]
+}
+//getStorage('delphinet', ungrund)
+//getStorage('delphinet', 'KT1VNKfd8dVzQ1rvkRDQrMfJgwGFUdMBcxdZ')
+
+const fullFillRequest = async (link) => {
+  return await axios.get(link).then(res => res.data)
+}
+
+const compare = async (request, response) => {
+
+  var response = await response
+  var allkeys = _.union(_.keys(request), _.keys(response))
+
+  var difference = _.reduce(allkeys, function (result, key) {
+    if (!_.isEqual(request[key], response[key])) {
+      result[key] = { request: request[key], response: response[key] }
+    }
+    return result
+  }, {})
+
+  const key = Object.keys(request).find(key => request[key] === "UNG")
+  //console.log(key)
+  //console.log(difference[key].response)
+  return difference[key].response
+
+}
+
+// PROCEDURE
+
+const mult = (response, factor) => {
+  return response * factor
+}
+
+const procedure = async () => {
+
+  var arr = await getStorage('delphinet', ungrund)
+  console.log(arr)
+  var requestResult = await fullFillRequest(arr[1][0])
+  console.log(requestResult)
+  var nat = await compare(JSON.parse(arr[1][2]), requestResult)
+  await inject(arr[0], nat * arr[1][1])
+
+}
+
+procedure()
+
+
+//let res = compare(JSON.parse('{"BRL":"UNG"}'), fullFillRequest('https://min-api.cryptocompare.com/data/price?fsym=USDT&tsyms=BRL'))
+//let res = compare(JSON.parse('{"randomness" : "UNG"}'), fullFillRequest('https://api.drand.sh/public/latest'))
+//console.log(mult(res, 1000))
+
+// HANDLE
